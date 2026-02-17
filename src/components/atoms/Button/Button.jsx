29faf0a6e28fcf2e355b5f6987e9c1d09e34785e
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-02-17 23:40:49 +0100

    Complete button markup + button.stories

---
+++ src/components/atoms/Button/Button.jsx
@@ -0,0 +1,5 @@
+import './_Button.scss';
+
+export default function Button() {
+    return <button className="button  text_preset_5--bold" type="button">continue</button>
+}
---
+++ src/components/atoms/Button/Button.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from './Button';
+
+export default {
+  title: 'Components/Atoms/Button',
+  component: Button,
+};
+
+export const Default = {
+  render: () => <Button />
+};
