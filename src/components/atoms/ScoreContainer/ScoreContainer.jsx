88f9498405f3d903711ec7f966de964ecb24e28d
unknown <tommaso.duranti191@gmail.com>
2026-02-19 18:24:56 +0100

    Add ScoreContainer.jsx, tbc

---
+++ src/components/atoms/ScoreContainer/ScoreContainer.jsx
@@ -0,0 +1,10 @@
+import "./_ScoreContainer.scss"
+
+export default function ScoreContainer() {
+    return (
+        <div className="score_container">
+            <h2 className="score_container__score  text_preset_2">72</h2>
+            <span className="score_container__compare  text_preset_6--bold">of 100</span>
+        </div>
+    )
+}
---
+++ src/components/atoms/ScoreContainer/ScoreContainer.stories.jsx
@@ -0,0 +1,10 @@
+import ScoreContainer from "./ScoreContainer";
+
+export default {
+    title: "Components/Atoms/ScoreContainer",
+    component: ScoreContainer,
+};
+
+export const Default = {
+    render: () => <ScoreContainer />
+};
