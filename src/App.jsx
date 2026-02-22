e8c25f22a11bc3e7afe34d03b811749982083e59
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-02-22 23:21:00 +0100

    Complete mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+import "./sass/main.scss"
+
+import ResultSection from './components/organisms/ResultSection/ResultSection';
+import SummarySection from './components/organisms/SummarySection/SummarySection';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
+    <div className="result_container">
+      <ResultSection />
+      <SummarySection />
+    </div>
   )
 }
 
